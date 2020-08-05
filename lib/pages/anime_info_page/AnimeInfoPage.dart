341a4bb602267d917e3249bf4b1e94b68b44f49e
simrat39 <simrats169169@gmail.com>
2020-08-05 11:56:00 -0700

    AnimeInfoPage: Update padding

--- lib/pages/anime_info_page/AnimeInfoPage.dart
+++ lib/pages/anime_info_page/AnimeInfoPage.dart
@@ -78,139 +78,141 @@ class _AnimeInfoPageState extends State<AnimeInfoPage> {
                 kitsuModel = snapshot.data;
               }
               return Scrollbar(
-                child: SingleChildScrollView(
+                child: ListView(
                   physics: BouncingScrollPhysics(),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Card(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15.0,
-                        ),
-                        child: Container(
-                          width: double.infinity,
-                          height: height * 0.35,
-                          child: Row(
-                            children: <Widget>[
-                              Container(
-                                width: width * 0.225,
-                                height: height * 0.35,
-                                child: Hero(
-                                  tag: widget.heroTag,
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(
-                                      8.0,
-                                    ),
-                                    child: Image(
-                                      image: NetworkImage(
-                                        kitsuModel.imageURL,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Card(
+                          margin: EdgeInsets.symmetric(
+                            horizontal: 15.0,
+                          ),
+                          child: Container(
+                            width: double.infinity,
+                            height: height * 0.35,
+                            child: Row(
+                              children: <Widget>[
+                                Container(
+                                  width: width * 0.225,
+                                  height: height * 0.35,
+                                  child: Hero(
+                                    tag: widget.heroTag,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(
+                                        8.0,
                                       ),
-                                      loadingBuilder:
-                                          (context, child, progress) {
-                                        if (progress == null) return child;
-                                        return Center(
-                                          child: CircularProgressIndicator(),
-                                        );
-                                      },
-                                      fit: BoxFit.cover,
+                                      child: Image(
+                                        image: NetworkImage(
+                                          kitsuModel.imageURL,
+                                        ),
+                                        loadingBuilder:
+                                            (context, child, progress) {
+                                          if (progress == null) return child;
+                                          return Center(
+                                            child: CircularProgressIndicator(),
+                                          );
+                                        },
+                                        fit: BoxFit.cover,
+                                      ),
                                     ),
                                   ),
                                 ),
+                                Expanded(
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: <Widget>[
+                                      RatingWidget(
+                                        kitsuModel: kitsuModel,
+                                      ),
+                                      WatchTrailerButton(
+                                        kitsuModel: kitsuModel,
+                                      ),
+                                      EpisodesButton(),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Card(
+                          margin: EdgeInsets.only(
+                            top: 10.0,
+                            left: 15.0,
+                            right: 15.0,
+                            bottom: 20.0,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(
+                                  top: 10.0,
+                                  left: 15.0,
+                                  right: 15.0,
+                                ),
+                                child: AutoSizeText(
+                                  widget.animeModel.title,
+                                  textAlign: TextAlign.start,
+                                  maxLines: 3,
+                                  minFontSize: 30.0,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontSize: 35.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ),
-                              Expanded(
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: <Widget>[
-                                    RatingWidget(
-                                      kitsuModel: kitsuModel,
+                              Padding(
+                                padding: EdgeInsets.only(
+                                  top: 10.0,
+                                ),
+                                child: Row(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                        left: 15.0,
+                                      ),
+                                      child: InfoChip(
+                                        text: "Season " +
+                                            widget.animeModel.season.toString(),
+                                      ),
                                     ),
-                                    WatchTrailerButton(
-                                      kitsuModel: kitsuModel,
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                        left: 10.0,
+                                      ),
+                                      child: InfoChip(
+                                        text: widget.animeModel.ongoing
+                                            ? "Ongoing"
+                                            : "Finished",
+                                      ),
                                     ),
-                                    EpisodesButton(),
                                   ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                  top: 15.0,
+                                  left: 15.0,
+                                  right: 15.0,
+                                  bottom: 20.0,
+                                ),
+                                child: DescriptionBox(
+                                  animeModel: widget.animeModel,
+                                  kitsuModel: kitsuModel,
                                 ),
                               ),
                             ],
                           ),
                         ),
-                      ),
-                      Card(
-                        margin: EdgeInsets.only(
-                          top: 10.0,
-                          left: 15.0,
-                          right: 15.0,
-                          bottom: 20.0,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(
-                                top: 15.0,
-                                left: 15.0,
-                                right: 15.0,
-                              ),
-                              child: AutoSizeText(
-                                widget.animeModel.title,
-                                textAlign: TextAlign.start,
-                                maxLines: 3,
-                                minFontSize: 30.0,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  fontSize: 35.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(
-                                top: 10.0,
-                              ),
-                              child: Row(
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.only(
-                                      left: 15.0,
-                                    ),
-                                    child: InfoChip(
-                                      text: "Season " +
-                                          widget.animeModel.season.toString(),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(
-                                      left: 10.0,
-                                    ),
-                                    child: InfoChip(
-                                      text: widget.animeModel.ongoing
-                                          ? "Ongoing"
-                                          : "Finished",
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(
-                                top: 15.0,
-                                left: 15.0,
-                                right: 15.0,
-                                bottom: 25.0,
-                              ),
-                              child: DescriptionBox(
-                                animeModel: widget.animeModel,
-                                kitsuModel: kitsuModel,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                      ],
+                    ),
+                  ],
                 ),
               );
             }
