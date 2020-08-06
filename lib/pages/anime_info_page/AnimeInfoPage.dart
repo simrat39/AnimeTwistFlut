fd64c2bfe701fd26e41fecee5efcbd2598cc303a
simrat39 <simrats169169@gmail.com>
2020-08-06 15:52:54 -0700

    AnimeInfoPage: Remove fade in animation for now
    bLoAt

--- lib/pages/anime_info_page/AnimeInfoPage.dart
+++ lib/pages/anime_info_page/AnimeInfoPage.dart
@@ -7,8 +7,6 @@ import '../../models/KitsuModel.dart';
 import '../../models/TwistModel.dart';
 import '../../models/EpisodeModel.dart';
 
-import '../../animations/FadeInSlide.dart';
-
 import '../../utils/KitsuUtils.dart';
 import '../../utils/EpisodeUtils.dart';
 
@@ -82,167 +80,164 @@ class _AnimeInfoPageState extends State<AnimeInfoPage> {
           future: _getKitsuModel,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-              return FadeIn(
-                child: Scrollbar(
-                  child: ScrollablePositionedList.builder(
-                    physics: BouncingScrollPhysics(),
-                    itemScrollController: _controller,
-                    itemCount: 2,
-                    itemBuilder: (context, index) {
-                      if (index == 0) {
-                        return Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Card(
-                              margin: EdgeInsets.symmetric(
-                                horizontal: 15.0,
-                              ),
-                              child: Container(
-                                width: double.infinity,
-                                height: height * 0.35,
-                                child: Row(
-                                  children: <Widget>[
-                                    Container(
-                                      width: width * 0.225,
-                                      height: height * 0.35,
-                                      child: Hero(
-                                        tag: widget.heroTag,
-                                        child: ClipRRect(
-                                          borderRadius: BorderRadius.circular(
-                                            8.0,
-                                          ),
-                                          child: Image(
-                                            image: NetworkImage(
-                                              kitsuModel.imageURL,
-                                            ),
-                                            loadingBuilder:
-                                                (context, child, progress) {
-                                              if (progress == null)
-                                                return child;
-                                              return Center(
-                                                child:
-                                                    CircularProgressIndicator(),
-                                              );
-                                            },
-                                            fit: BoxFit.cover,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    Expanded(
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceEvenly,
-                                        mainAxisSize: MainAxisSize.max,
-                                        children: <Widget>[
-                                          RatingWidget(
-                                            kitsuModel: kitsuModel,
-                                          ),
-                                          WatchTrailerButton(
-                                            kitsuModel: kitsuModel,
-                                          ),
-                                          EpisodesButton(
-                                            episodes: episodes,
-                                            controller: _controller,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
+              return Scrollbar(
+                child: ScrollablePositionedList.builder(
+                  physics: BouncingScrollPhysics(),
+                  itemScrollController: _controller,
+                  itemCount: 2,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Card(
+                            margin: EdgeInsets.symmetric(
+                              horizontal: 15.0,
                             ),
-                            Card(
-                              margin: EdgeInsets.only(
-                                top: 10.0,
-                                left: 15.0,
-                                right: 15.0,
-                                bottom: 10.0,
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.only(
-                                      top: 10.0,
-                                      left: 15.0,
-                                      right: 15.0,
-                                    ),
-                                    child: AutoSizeText(
-                                      widget.twistModel.title,
-                                      textAlign: TextAlign.start,
-                                      maxLines: 3,
-                                      minFontSize: 30.0,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                        fontSize: 35.0,
-                                        fontWeight: FontWeight.bold,
+                            child: Container(
+                              width: double.infinity,
+                              height: height * 0.35,
+                              child: Row(
+                                children: <Widget>[
+                                  Container(
+                                    width: width * 0.225,
+                                    height: height * 0.35,
+                                    child: Hero(
+                                      tag: widget.heroTag,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(
+                                          8.0,
+                                        ),
+                                        child: Image(
+                                          image: NetworkImage(
+                                            kitsuModel.imageURL,
+                                          ),
+                                          loadingBuilder:
+                                              (context, child, progress) {
+                                            if (progress == null) return child;
+                                            return Center(
+                                              child:
+                                                  CircularProgressIndicator(),
+                                            );
+                                          },
+                                          fit: BoxFit.cover,
+                                        ),
                                       ),
                                     ),
                                   ),
-                                  Padding(
-                                    padding: EdgeInsets.only(
-                                      top: 10.0,
-                                    ),
-                                    child: Row(
-                                      children: [
-                                        Padding(
-                                          padding: EdgeInsets.only(
-                                            left: 15.0,
-                                          ),
-                                          child: InfoChip(
-                                            text: "Season " +
-                                                widget.twistModel.season
-                                                    .toString(),
-                                          ),
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: <Widget>[
+                                        RatingWidget(
+                                          kitsuModel: kitsuModel,
                                         ),
-                                        Padding(
-                                          padding: EdgeInsets.only(
-                                            left: 10.0,
-                                          ),
-                                          child: InfoChip(
-                                            text: widget.twistModel.ongoing
-                                                ? "Ongoing"
-                                                : "Finished",
-                                          ),
+                                        WatchTrailerButton(
+                                          kitsuModel: kitsuModel,
+                                        ),
+                                        EpisodesButton(
+                                          episodes: episodes,
+                                          controller: _controller,
                                         ),
                                       ],
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(
-                                      top: 15.0,
-                                      left: 15.0,
-                                      right: 15.0,
-                                      bottom: 20.0,
-                                    ),
-                                    child: DescriptionBox(
-                                      twistModel: widget.twistModel,
-                                      kitsuModel: kitsuModel,
                                     ),
                                   ),
                                 ],
                               ),
                             ),
-                          ],
-                        );
-                      } else if (index == 1) {
-                        return Padding(
-                          padding: EdgeInsets.only(
-                            left: 15.0,
-                            right: 15.0,
-                            bottom: 20.0,
                           ),
-                          child: EpisodesCard(
-                            episodes: episodes,
-                            twistModel: widget.twistModel,
+                          Card(
+                            margin: EdgeInsets.only(
+                              top: 10.0,
+                              left: 15.0,
+                              right: 15.0,
+                              bottom: 10.0,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    top: 10.0,
+                                    left: 15.0,
+                                    right: 15.0,
+                                  ),
+                                  child: AutoSizeText(
+                                    widget.twistModel.title,
+                                    textAlign: TextAlign.start,
+                                    maxLines: 3,
+                                    minFontSize: 30.0,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontSize: 35.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    top: 10.0,
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                          left: 15.0,
+                                        ),
+                                        child: InfoChip(
+                                          text: "Season " +
+                                              widget.twistModel.season
+                                                  .toString(),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                          left: 10.0,
+                                        ),
+                                        child: InfoChip(
+                                          text: widget.twistModel.ongoing
+                                              ? "Ongoing"
+                                              : "Finished",
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    top: 15.0,
+                                    left: 15.0,
+                                    right: 15.0,
+                                    bottom: 20.0,
+                                  ),
+                                  child: DescriptionBox(
+                                    twistModel: widget.twistModel,
+                                    kitsuModel: kitsuModel,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        );
-                      }
-                      return Container();
-                    },
-                  ),
+                        ],
+                      );
+                    } else if (index == 1) {
+                      return Padding(
+                        padding: EdgeInsets.only(
+                          left: 15.0,
+                          right: 15.0,
+                          bottom: 20.0,
+                        ),
+                        child: EpisodesCard(
+                          episodes: episodes,
+                          twistModel: widget.twistModel,
+                        ),
+                      );
+                    }
+                    return Container();
+                  },
                 ),
               );
             }
