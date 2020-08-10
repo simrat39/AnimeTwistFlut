5d40160af0cca51f609591ad8f0d564691a68393
simrat39 <simrats169169@gmail.com>
2020-08-10 13:44:30 -0700

    MOTDCard: Allow removing message of the day

--- lib/pages/homepage/HomePage.dart
+++ lib/pages/homepage/HomePage.dart
@@ -149,14 +149,7 @@ class _HomePageState extends State<HomePage> {
               child: Column(
                 children: [
                   // Message Of The Day Card
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: 15.0,
-                      right: 15.0,
-                      bottom: 15.0,
-                    ),
-                    child: MOTDCard(),
-                  ),
+                  MOTDCard(),
                   // Explore anime slider
                   Padding(
                     padding: EdgeInsets.only(
--- lib/pages/homepage/MOTDCard.dart
+++ lib/pages/homepage/MOTDCard.dart
@@ -1,5 +1,6 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:supercharged/supercharged.dart';
 
 // Project imports:
 import '../../utils/MOTDUtils.dart';
@@ -13,6 +14,7 @@ class MOTDCard extends StatefulWidget {
 
 class _MOTDCardState extends State<MOTDCard> {
   Future<List<String>> _dataInit;
+  bool shouldShow = true;
 
   @override
   void initState() {
@@ -26,51 +28,89 @@ class _MOTDCardState extends State<MOTDCard> {
       future: _dataInit,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done)
-          return Card(
-            child: Container(
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
+          return AnimatedSwitcher(
+            duration: 500.milliseconds,
+            child: shouldShow
+                ? Padding(
                     padding: EdgeInsets.only(
                       left: 15.0,
                       right: 15.0,
-                      top: 15.0,
-                      bottom: 10.0,
+                      bottom: 15.0,
                     ),
-                    child: Text(
-                      snapshot.data[0],
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Theme.of(context).accentColor,
-                        fontWeight: FontWeight.bold,
+                    child: Card(
+                      child: Container(
+                        width: double.infinity,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: 15.0,
+                                right: 15.0,
+                                top: 15.0,
+                                bottom: 10.0,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(
+                                    snapshot.data[0],
+                                    textAlign: TextAlign.start,
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      color: Theme.of(context).accentColor,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        shouldShow = false;
+                                      });
+                                    },
+                                    child: Icon(
+                                      Icons.close_rounded,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                bottom: 20.0,
+                                left: 15.0,
+                                right: 15.0,
+                              ),
+                              child: Text(
+                                snapshot.data[1],
+                                textAlign: TextAlign.start,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      bottom: 20.0,
-                      left: 15.0,
-                      right: 15.0,
-                    ),
-                    child: Text(
-                      snapshot.data[1],
-                      textAlign: TextAlign.start,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+                  )
+                : Container(),
           );
-        return Card(
-          child: Center(
-            child: Padding(
-              padding: EdgeInsets.all(
-                30.0,
+        return Padding(
+          padding: EdgeInsets.only(
+            left: 15.0,
+            right: 15.0,
+            bottom: 15.0,
+          ),
+          child: Card(
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(
+                  30.0,
+                ),
+                child: Transform.scale(
+                  scale: 0.6,
+                  child: CircularProgressIndicator(),
+                ),
               ),
-              child: CircularProgressIndicator(),
             ),
           ),
         );
