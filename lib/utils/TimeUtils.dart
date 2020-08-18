5e5482e2186de38db8f1e81976a6f2dc6e7c28fb
simrat39 <simrats169169@gmail.com>
2020-08-18 14:34:13 -0700

    TimeUtils: Parse > 1 hour times correctly

--- lib/utils/TimeUtils.dart
+++ lib/utils/TimeUtils.dart
@@ -1,9 +1,18 @@
+import 'package:supercharged/supercharged.dart';
+
 class TimeUtils {
   static String secondsToHumanReadable(int sec) {
     String minutes = (sec ~/ 60).toString();
     String seconds = (sec % 60).toString();
     if (seconds.length == 1) seconds = "0$seconds";
     if (minutes.length == 1) minutes = "0$minutes";
+    if (sec > 1.hours.inSeconds) {
+      String hours = (int.parse(minutes) ~/ 60).toString();
+      minutes = (int.parse(minutes) - 1.minutes.inSeconds.toInt()).toString();
+      if (hours.length == 1) hours = "0$hours";
+      if (minutes.length == 1) minutes = "0$minutes";
+      return "$hours:$minutes:$seconds";
+    }
     return "$minutes:$seconds";
   }
 
