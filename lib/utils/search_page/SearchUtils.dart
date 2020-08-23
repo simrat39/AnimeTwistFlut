7cad700c7c2c7e0814ab47ebe5c64e411ef8663f
simrat39 <simrats169169@gmail.com>
2020-08-22 21:30:23 -0700

    SearchUtils: Remove whitespace from title when searching

--- lib/utils/search_page/SearchUtils.dart
+++ lib/utils/search_page/SearchUtils.dart
@@ -4,12 +4,21 @@ import '../../models/TwistModel.dart';
 class SearchUtils {
   static bool isTextInAnimeModel({String text, TwistModel twistModel}) {
     if (text.isEmpty ||
-        twistModel.title.toLowerCase().contains(text.toLowerCase()) ||
-        (twistModel.altTitle ?? twistModel.title)
+        twistModel.title
+            .removeWhitespace()
+            .toLowerCase()
+            .contains(text.toLowerCase()) ||
+        (twistModel.altTitle?.removeWhitespace() ?? twistModel.title)
             .toLowerCase()
             .contains(text.toLowerCase())) {
       return true;
     }
     return false;
+  }
+}
+
+extension StringExtensions on String {
+  String removeWhitespace() {
+    return this.replaceAll(' ', '');
   }
 }
