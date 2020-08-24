96c4221959770a910190182967f092ae48e24df5
simrat39 <simrats169169@gmail.com>
2020-08-23 21:18:36 -0700

    Fuzzy Search: Improve performance

--- lib/utils/search_page/SearchUtils.dart
+++ lib/utils/search_page/SearchUtils.dart
@@ -4,6 +4,10 @@ import 'package:fuzzy/fuzzy.dart';
 
 // Project imports:
 import '../../models/TwistModel.dart';
+import '../../utils/TwistUtils.dart';
+
+// Project imports:
+
 
 class SearchUtils {
   static bool isTextInAnimeModel({String text, TwistModel twistModel}) {
@@ -15,10 +19,26 @@ class SearchUtils {
     }
     return false;
   }
-}
 
-extension StringExtensions on String {
-  String removeWhitespace() {
-    return this.replaceAll(' ', '');
+  static List<TwistModel> getSearchResults({String text}) {
+    final fuse = Fuzzy(TwistUtils.allTwistModel.map((model) {
+      return model.title;
+    }).toList());
+
+    List<Result<String>> res = fuse.search(text);
+
+    List<TwistModel> ret = [];
+    for (int i = 0; i < res.length; i++) {
+      if (res.elementAt(i).score < 0.5) {
+        for (int j = 0; j < TwistUtils.allTwistModel.length; j++) {
+          TwistModel twistModel = TwistUtils.allTwistModel[j];
+          if (twistModel.title == res.elementAt(i).item) {
+            ret.add(twistModel);
+            break;
+          }
+        }
+      }
+    }
+    return ret;
   }
 }
