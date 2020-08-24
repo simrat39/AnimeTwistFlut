bcba609146462bd45439ffe5bf512b9953f11397
simrat39 <simrats169169@gmail.com>
2020-08-23 19:32:35 -0700

    Search: Implement basic fuzzy search

--- lib/utils/search_page/SearchUtils.dart
+++ lib/utils/search_page/SearchUtils.dart
@@ -1,16 +1,16 @@
+// Package imports:
+import 'package:fuzzy/data/result.dart';
+import 'package:fuzzy/fuzzy.dart';
+
 // Project imports:
 import '../../models/TwistModel.dart';
 
 class SearchUtils {
   static bool isTextInAnimeModel({String text, TwistModel twistModel}) {
-    if (text.isEmpty ||
-        twistModel.title
-            .removeWhitespace()
-            .toLowerCase()
-            .contains(text.toLowerCase()) ||
-        (twistModel.altTitle?.removeWhitespace() ?? twistModel.title)
-            .toLowerCase()
-            .contains(text.toLowerCase())) {
+    final fuse =
+        Fuzzy([twistModel.title, twistModel.altTitle ?? twistModel.title]);
+    List<Result<String>> res = fuse.search(text);
+    if (text.isEmpty || res.isNotEmpty && res[0].score < 0.3) {
       return true;
     }
     return false;
