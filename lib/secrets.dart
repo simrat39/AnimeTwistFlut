75beb642a38d65eda3e4c9e1fb4190002f70a4d6
simrat39 <simrats169169@gmail.com>
2020-08-27 12:31:22 -0700

    Secrets: Add a working default value to token
    The official app does this also so its not that secret anyways, also allows us to have tests

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,2 +1,3 @@
-const String x_access_token = String.fromEnvironment('TOKEN', defaultValue: "");
+const String x_access_token = String.fromEnvironment('TOKEN',
+    defaultValue: "1rj2vRtegS8Y60B3w3qNZm5T2Q0TN2NR");
 const String key = String.fromEnvironment('KEY', defaultValue: '');
