5b2e2b71364ba9f693c944133ba5ec753d631720
simrat39 <simrats169169@gmail.com>
2021-01-29 19:20:35 -0800

    Remove unused Fuzzy dependency

--- lib/utils/search_page/SearchUtils.dart
+++ lib/utils/search_page/SearchUtils.dart
@@ -1,11 +1,8 @@
 // Package imports:
 import 'package:edit_distance/edit_distance.dart';
-import 'package:fuzzy/data/result.dart';
-import 'package:fuzzy/fuzzy.dart';
 
 // Project imports:
 import '../../models/TwistModel.dart';
-import '../../services/twist_service/TwistApiService.dart';
 
 // Project imports:
 
@@ -22,28 +19,6 @@ class SearchUtils {
         l.normalizedDistance(_title, _text) < 0.3 ||
         l.normalizedDistance(_altTitle, _text) < 0.3) return true;
     return false;
-  }
-
-  static List<TwistModel> getSearchResults({String text}) {
-    final fuse = Fuzzy(TwistApiService.allTwistModel.map((model) {
-      return model.title;
-    }).toList());
-
-    List<Result<String>> res = fuse.search(text);
-
-    List<TwistModel> ret = [];
-    for (int i = 0; i < res.length; i++) {
-      if (res.elementAt(i).score < 0.5) {
-        for (int j = 0; j < TwistApiService.allTwistModel.length; j++) {
-          TwistModel twistModel = TwistApiService.allTwistModel[j];
-          if (twistModel.title == res.elementAt(i).item) {
-            ret.add(twistModel);
-            break;
-          }
-        }
-      }
-    }
-    return ret;
   }
 }
 
