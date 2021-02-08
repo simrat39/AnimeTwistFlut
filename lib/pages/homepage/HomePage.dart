1e9f85396e12f31dc71b50b9e1bca905a1b4f1a3
simrat39 <simrats169169@gmail.com>
2021-02-07 18:37:33 -0800

    HomePage: Move uni_links code to RootWindow
    * Because whenever we changed orientation, HomePage was being rebuilt
      which caused this code to run again and launch a new anime info page
      on every orientation which is obv not desired.

--- lib/pages/homepage/HomePage.dart
+++ lib/pages/homepage/HomePage.dart
@@ -1,28 +1,15 @@
-// Flutter imports:
-import 'dart:async';
-
-import 'package:anime_twist_flut/models/TwistModel.dart';
-import 'package:anime_twist_flut/pages/anime_info_page/AnimeInfoPage.dart';
 import 'package:anime_twist_flut/pages/homepage/HomePageLandscape.dart';
 import 'package:anime_twist_flut/pages/homepage/HomePagePortrait.dart';
 import 'package:anime_twist_flut/pages/homepage/to_watch_row/ToWatchRow.dart';
 import 'package:anime_twist_flut/services/AppUpdateService.dart';
 import 'package:anime_twist_flut/widgets/device_orientation_builder.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-// Package imports:
-import 'package:supercharged/supercharged.dart';
-
-// Project imports:
-import '../../animations/Transitions.dart';
-import '../../services/twist_service/TwistApiService.dart';
 import 'recently_watched_slider/RecentlyWatchedSlider.dart';
 import 'MOTDCard.dart';
 import 'ViewAllAnimeCard.dart';
 import 'donation_card/DonationCard.dart';
 import 'explore_slider/ExploreSlider.dart';
-import 'package:uni_links/uni_links.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -33,81 +20,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with AutomaticKeepAliveClientMixin {
-  StreamSubscription _uriSub;
-
-  @override
-  void initState() {
-    // Check and launch an anime page on initial app launch, this is needed when
-    // the app is not running in the background and the user clicks a relevant
-    // link.
-    _checkURIAndLaunchPage();
-    // Check and launch a page every times a user clicks on a relevant link.
-    _uriSub = getLinksStream().listen((String url) {
-      _checkURIAndLaunchPage(url);
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _uriSub.cancel();
-    super.dispose();
-  }
-
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     AppUpdateService appUpdateService = AppUpdateService();
     appUpdateService.checkUpdate(context: context);
-  }
-
-  Future _checkURIAndLaunchPage([String url]) async {
-    // Wait for initData() to finish fetching and making all the anime models
-    while (TwistApiService.allTwistModel.isEmpty) {
-      await Future.delayed(200.milliseconds);
-    }
-
-    try {
-      String recievedUrl = (url ?? await getInitialLink()) ?? "";
-      RegExp regex = RegExp(r'https://twist.moe/a/(.*)/+(.*)');
-      Iterable<Match> matches = regex.allMatches(recievedUrl);
-      String slug = matches.length > 0 ? matches.elementAt(0).group(1) : "";
-      int episodeNum =
-          matches.length > 0 ? int.parse(matches.elementAt(0).group(2)) : null;
-
-      if (slug.isNotEmpty) {
-        for (int i = 0; i < TwistApiService.allTwistModel.length; i++) {
-          TwistModel twistModel = TwistApiService.allTwistModel.elementAt(i);
-          if (twistModel.slug == slug) {
-            // If the current page is home page, then launch the anime page
-            // normally, but if its any other page, then launch replaced as we
-            // dont want to nest a lot of pages, if users spams the same link
-            // again and again.
-            if (ModalRoute.of(context).isCurrent) {
-              Transitions.slideTransition(
-                context: context,
-                pageBuilder: () => AnimeInfoPage(
-                  twistModel: twistModel,
-                  isFromRecentlyWatched: episodeNum > 1 ? true : false,
-                  lastWatchedEpisodeNum: episodeNum,
-                ),
-              );
-            } else {
-              Transitions.slideTransitionReplaced(
-                context: context,
-                pageBuilder: () => AnimeInfoPage(
-                  twistModel: twistModel,
-                  isFromRecentlyWatched: episodeNum > 1 ? true : false,
-                  lastWatchedEpisodeNum: episodeNum,
-                ),
-              );
-            }
-          }
-        }
-      }
-    } on PlatformException {
-      // handle later
-    }
   }
 
   final List<Widget> widgets = [
--- lib/pages/root_window/root_window.dart
+++ lib/pages/root_window/root_window.dart
@@ -1,10 +1,19 @@
+import 'dart:async';
+
+import 'package:anime_twist_flut/animations/Transitions.dart';
+import 'package:anime_twist_flut/models/TwistModel.dart';
+import 'package:anime_twist_flut/pages/anime_info_page/AnimeInfoPage.dart';
 import 'package:anime_twist_flut/pages/favourites_page/FavouritesPage.dart';
 import 'package:anime_twist_flut/pages/homepage/HomePage.dart';
 import 'package:anime_twist_flut/pages/root_window/root_window_landscape.dart';
 import 'package:anime_twist_flut/pages/root_window/root_window_portrait.dart';
+import 'package:anime_twist_flut/services/twist_service/TwistApiService.dart';
 import 'package:anime_twist_flut/widgets/device_orientation_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/all.dart';
+import 'package:uni_links/uni_links.dart';
+import 'package:supercharged_dart/supercharged_dart.dart';
 
 class RootWindow extends StatefulWidget {
   RootWindow({Key key}) : super(key: key);
@@ -15,6 +24,8 @@ class RootWindow extends StatefulWidget {
 
 class _RootWindowState extends State<RootWindow> with TickerProviderStateMixin {
   TabController _tabController;
+  StreamSubscription _uriSub;
+
   var pages = [HomePage(), FavouritesPage()];
   final indexProvider = StateProvider<int>((ref) => 0);
 
@@ -22,6 +33,7 @@ class _RootWindowState extends State<RootWindow> with TickerProviderStateMixin {
   void dispose() {
     super.dispose();
 
+    _uriSub.cancel();
     _tabController.dispose();
   }
 
@@ -29,7 +41,64 @@ class _RootWindowState extends State<RootWindow> with TickerProviderStateMixin {
   void initState() {
     super.initState();
 
+    // Check and launch an anime page on initial app launch, this is needed when
+    // the app is not running in the background and the user clicks a relevant
+    // link.
+    _checkURIAndLaunchPage();
+    // Check and launch a page every times a user clicks on a relevant link.
+    _uriSub = getLinksStream().listen((String url) {
+      _checkURIAndLaunchPage(url);
+    });
     _tabController = TabController(length: pages.length, vsync: this);
+  }
+
+  Future _checkURIAndLaunchPage([String url]) async {
+    // Wait for initData() to finish fetching and making all the anime models
+    while (TwistApiService.allTwistModel.isEmpty) {
+      await Future.delayed(200.milliseconds);
+    }
+
+    try {
+      String recievedUrl = (url ?? await getInitialLink()) ?? "";
+      RegExp regex = RegExp(r'https://twist.moe/a/(.*)/+(.*)');
+      Iterable<Match> matches = regex.allMatches(recievedUrl);
+      String slug = matches.length > 0 ? matches.elementAt(0).group(1) : "";
+      int episodeNum =
+          matches.length > 0 ? int.parse(matches.elementAt(0).group(2)) : null;
+
+      if (slug.isNotEmpty) {
+        for (int i = 0; i < TwistApiService.allTwistModel.length; i++) {
+          TwistModel twistModel = TwistApiService.allTwistModel.elementAt(i);
+          if (twistModel.slug == slug) {
+            // If the current page is home page, then launch the anime page
+            // normally, but if its any other page, then launch replaced as we
+            // dont want to nest a lot of pages, if users spams the same link
+            // again and again.
+            if (ModalRoute.of(context).isCurrent) {
+              Transitions.slideTransition(
+                context: context,
+                pageBuilder: () => AnimeInfoPage(
+                  twistModel: twistModel,
+                  isFromRecentlyWatched: episodeNum > 1 ? true : false,
+                  lastWatchedEpisodeNum: episodeNum,
+                ),
+              );
+            } else {
+              Transitions.slideTransitionReplaced(
+                context: context,
+                pageBuilder: () => AnimeInfoPage(
+                  twistModel: twistModel,
+                  isFromRecentlyWatched: episodeNum > 1 ? true : false,
+                  lastWatchedEpisodeNum: episodeNum,
+                ),
+              );
+            }
+          }
+        }
+      }
+    } on PlatformException {
+      // handle later
+    }
   }
 
   @override
