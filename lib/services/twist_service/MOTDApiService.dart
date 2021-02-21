eda2c6c1c92f563d40a9a4c6b49649a4f180bc03
simrat39 <simrats169169@gmail.com>
2021-02-21 13:56:48 -0800

    MOTDApiService: Cache data and update every 7 days

--- lib/services/twist_service/MOTDApiService.dart
+++ lib/services/twist_service/MOTDApiService.dart
@@ -5,22 +5,41 @@ import 'dart:convert';
 
 // Package imports:
 import 'package:anime_twist_flut/cached_http_get/CachedHttpGet.dart';
+import 'package:anime_twist_flut/services/CacheService.dart';
 import 'package:anime_twist_flut/services/twist_service/TwistApiService.dart';
+import 'package:anime_twist_flut/utils/JsonUtils.dart';
 import 'package:flutter/foundation.dart';
+import 'package:supercharged/supercharged.dart';
 
 // Project imports:
 import '../../secrets.dart';
 
 class MOTDApiService {
   static Future<List<String>> getMOTD() async {
-    String response = await CachedHttpGet.get(
-      Request(
-        url: TwistApiService.BASE_API_URL + '/motd',
-        header: {
-          'x-access-token': x_access_token,
-        },
-      ),
+    CacheService cacheService = CacheService(
+      "/data/motd",
+      7.days,
     );
+
+    await cacheService.initialize(false);
+    String response = await cacheService.getDataAndCacheIfNeeded(
+      getData: () async {
+        return await CachedHttpGet.get(
+          Request(
+            url: TwistApiService.BASE_API_URL + '/motd',
+            header: {
+              'x-access-token': x_access_token,
+            },
+          ),
+        );
+      },
+      onCache: () {},
+      onSkipCache: () {},
+      willUpdateCache: (cachedData, __) async {
+        return !JsonUtils.isValidJson(cachedData);
+      },
+    );
+
     return await compute(_computeData, response);
   }
 
